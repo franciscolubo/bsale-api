75fb7c049a92edb373edc8612c6eab4dc2b14ba8
LuboFrancisco <lupanchox@gmail.com>
2022-09-26 16:53:32 -0300

    complete api endpoints

--- src/db.js
+++ src/db.js
@@ -1,16 +1,18 @@
 const mysql = require('mysql')
 require('dotenv').config()
 
+// Creo un objeto con la informacion de mi db_config, a traves de variables de entorno
+
 const db_config = {
     host: process.env.HOST,
     user: process.env.USER,
+    port: 3306,
     password: process.env.PASSWORD,
     database: process.env.DATABASE
 }
 
 
 const pool = mysql.createPool(db_config)
-
 const conectionDB = () => {
     pool.getConnection((err, connection) => {
         if (err) {
@@ -21,19 +23,21 @@ const conectionDB = () => {
     })
 }
 
+// Realizo las distintas sentencias para MySQL con las siguientes funciones
+
 const callProducts = (callback) => {
     const SQL = 'SELECT * FROM product'
-    pool.query(SQL, (err, results, fields) => {
+    pool.query(SQL, (err, results) => {
         if (err) {
             throw err
         }
-        callback(results)
+        callback(results) // Utilizo un callback para el manejo de los datos
     })
 }
 
 const callCategories = (callback) => {
     const SQL = 'SELECT * FROM category'
-    pool.query(SQL, (err, results, fields) => {
+    pool.query(SQL, (err, results) => {
         if (err) {
             throw err
         }
@@ -43,7 +47,7 @@ const callCategories = (callback) => {
 
 const callByCategory = (value, callback) => {
     const SQL = `SELECT * FROM product WHERE category = ${value}`
-    pool.query(SQL, (err, results, fields) => {
+    pool.query(SQL, (err, results) => {
         if (err) {
             throw err
         }
@@ -51,6 +55,24 @@ const callByCategory = (value, callback) => {
     })
 }
 
+const callOrder = (order, type, callback) => {
+    const SQL = `SELECT * FROM product ORDER BY ${type} ${order}`
+    pool.query(SQL, (err, results) => {
+        if (err) {
+            throw err
+        }
+        callback(results)
+    })
+}
 
+const searchBar = (value, callback) => {
+    const SQL = `SELECT * FROM product WHERE name LIKE '${value}%'`
+    pool.query(SQL, (err, results) => {
+        if (err) {
+            throw err
+        }
+        callback(results)
+    })
+}
 
-module.exports = { conectionDB, callProducts, callCategories, callByCategory }
+module.exports = { conectionDB, callProducts, callCategories, callByCategory, callOrder, searchBar }
